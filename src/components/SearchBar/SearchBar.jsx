e613a4e684d0cf3e0f2f1f40db77c4adee35ec53
Joao Brugnado <joao.psb20@gmail.com>
2024-01-19 17:24:17 -0300

    Adicionado o context Provider para pesquisa de produtos

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { BsSearch } from 'react-icons/bs';
+import fetchProducts from '../../api/fetchProducts';
+import AppContext from '../../context/AppContext';
 
 import './SearchBar.css';
 
 const SearchBar = () => {
   const [searchValue, setSearchValue] = useState('');
 
+  //Context
+  const {setProducts, setLoading} = useContext(AppContext)
+
+  const Search = async (event) => {
+    event.preventDefault();
+    setLoading(true)
+    const products = await fetchProducts(searchValue)
+    setProducts(products)
+    setLoading(false)
+    setSearchValue('')
+  }
+
   function handleSearch({ target }) {
     setSearchValue(target.value)
   }
 
   return (
-    <form className='search_bar'>
+    <form className='search_bar' onSubmit={Search}>
       <input type="search" placeholder='Buscar produtos' className='search_input' value={searchValue} onChange={handleSearch} required />
       <button type='submit' className='search_button'>
         <BsSearch />
